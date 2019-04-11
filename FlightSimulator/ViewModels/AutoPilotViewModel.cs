2708536e319ee56db5ab60b25b718ac2dba0850a
Erez Weiss <erezweiss0@gmail.com>
2019-04-11 17:05:47 +0300

    ViewModel Updated

---
+++ FlightSimulator/ViewModels/AutoPilotViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightSimulator.ViewModels
+{
+    class AutoPilotViewModel : BaseNotify
+    {
+        string a;
+        public string Commands {
+            get {
+                return "hello";
+            }
+            set
+            {
+                a = value;
+                NotifyPropertyChanged("ServerIP");
+                Console.WriteLine("Hello World!");
+            }
+        }
+    }
+}
---
+++ FlightSimulator/ViewModels/JoystickWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightSimulator.ViewModels
+{
+    class JoystickWindowViewModel : BaseNotify
+    {
+        public double Throttle
+        {
+            get;
+            set;
+        }
+        public double Rudder
+        {
+            get;
+            set;
+        }
+    }
+}
